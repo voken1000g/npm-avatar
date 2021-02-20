e94eced469b565e55dddcde16057c6fed4ad974f
Lyman Snow <pmvoken@protonmail.com>
2021-02-20 16:32:50 +0000

    auto publish

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+const crypto = require('crypto')
 const Avatars = require('@dicebear/avatars').default
-const sprites = require('@dicebear/avatars-gridy-sprites').default
+// const sprites = require('@dicebear/avatars-gridy-sprites').default
+const sprites = require('@dicebear/avatars-jdenticon-sprites').default
 
 const svgFromSeed = function (seed) {
-  const options = {
-    colorful: true,
-    deterministic: true
-  }
-  const avatars = new Avatars(sprites, options)
-  return avatars.create(seed)
+  // const options = {
+  //   colorful: true,
+  //   deterministic: true
+  // }
+  // const avatars = new Avatars(sprites, options)
+  const avatars = new Avatars(sprites)
+  return avatars.create(_sha256(seed))
 }
 
 const svgDataUriFromSeed = function (seed) {
   const options = {
     dataUri: true,
-    colorful: true,
-    deterministic: true
+    // colorful: true,
+    // deterministic: true
   }
   const avatars = new Avatars(sprites, options)
-  return avatars.create(seed)
+  return avatars.create(_sha256(seed))
+}
+
+const _sha256 = function (value) {
+  return crypto.createHash('sha256').update(value).digest().toString('hex')
 }
 
 module.exports = {
